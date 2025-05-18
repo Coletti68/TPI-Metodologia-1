8818e2fd2ae13a45909a6c986c138fa364243959
MarcosRoa03 <Marcosrooa17@gmail.com>
2025-05-17 21:50:55 -0300

    Se corrigio error en vista solicitar turno al ejeutar con node,.s
    Al presionar el boton solicitar turno mostraba solamente el html, ahora muestra el html + el css

--- Proyecto/Backend/server.js
+++ Proyecto/Backend/server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3002;
+const PORT = 3001;
 
-// Servir archivos estáticos (HTML, CSS, imágenes, etc.)
+// 👉 Servir archivos estáticos desde la carpeta Front
 app.use(express.static(path.join(__dirname, '../Front')));
 
-// Ruta principal opcional
+
+// 👉 Ruta para abrir el archivo Home_No_Login.html por defecto
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../Front/Home_No_Login.html'));
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
