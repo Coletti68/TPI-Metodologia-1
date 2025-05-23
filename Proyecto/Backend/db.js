fad12b6609407c322c7c5ec9a9938deeb2850e55
MarcosRoa03 <Marcosrooa17@gmail.com>
2025-05-22 23:58:42 -0300

    Se agrego vista para ver y guardar turnos.
    vista ver turnos con los turnos solicitados y el historial de turnos.

--- Proyecto/Backend/db.js
+++ Proyecto/Backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',           // Su nombre de usuario de mysql
-  password: 'Facundo1',           // contraseña de MySQL 
+  password: 'marcosroa03',           // contraseña de MySQL 
   database: 'SaludTotalBDD' // nombre de la base de datos
 });
 
