c53e6361bddc2df8f1729eb6908c18a344c427ce
Coletti68 <facucoletti11@gmail.com>
2025-06-16 17:44:07 -0300

    cambio de imagenes

--- Proyecto/Backend/Routes/patientRoutes.js
+++ Proyecto/Backend/Routes/patientRoutes.js
@@ -3,10 +3,8 @@ const router = express.Router();
 const patientController = require('../Controllers/patientController'); 
 console.log(patientController);
 
-//registro de paciente
 router.post('/register',patientController.register);
 
-//Login de paciente
 router.post('/login',patientController.login);
 
 router.get('/test-connection', (req, res) => {
